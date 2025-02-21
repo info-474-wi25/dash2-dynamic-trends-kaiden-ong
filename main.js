38f9d20238f3eb242a72daf2005564df1283381c
ryanngu023 <ryannguyen10023@gmail.com>
2025-02-20 21:35:25 -0800

    transformations for chart 1

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const width = 900 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
 // Create SVG containers for both charts
-const svg1_RENAME = d3.select("#lineChart1") // If you change this ID, you must change it in index.html too
+const svg1_precipitation = d3.select("#lineChart1") // If you change this ID, you must change it in index.html too
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -22,17 +22,47 @@ const svg2_RENAME = d3.select("#lineChart2")
 // const tooltip = ...
 
 // 2.a: LOAD...
-d3.csv("YOUR_CSV_NAME.csv").then(data => {
+d3.csv("weather.csv").then(data => {
     // 2.b: ... AND TRANSFORM DATA
+    console.log(data);
+    const parseDate = d3.timeParse("%m/%d/%Y");
+    const formatMonthYear = d3.timeFormat("%B-%Y");
 
+    data.forEach(d => {
+        d.date = parseDate(d.date);
+        d["month-year"] = formatMonthYear(d.date);
+        d.actual_precipitation = +d.actual_precipitation; // Ensure this is a number
+    });
+
+    console.log("Parsed Data:", data);
+
+    const nestedData = d3.group(data, d => d.city, d => d["month-year"]);
+    const monthlyAverages = Array.from(nestedData, ([city, months]) => ({
+        city,
+        values: Array.from(months, ([monthYear, records]) => ({
+            monthYear,
+            avgPrecipitation: d3.mean(records, d => d.actual_precipitation)
+        }))
+    }));
+    console.log(monthlyAverages);
     // 3.a: SET SCALES FOR CHART 1
+    const xScale = d3.scaleTime()
+        .domain(d3.extent(data, d => d.date))
+        .range([0, width]);
 
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(monthlyAverages, city => d3.max(city.values, d => d.avgPrecipitation))])
+        .range([height, 0]);
 
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10)
+        .domain(monthlyAverages.map(d => d.city));
+
+    const line = d3.line()
+        .x(d => xScale(parseDate(d.monthYear)))
+        .y(d => yScale(d.avgPrecipitation));
     // 4.a: PLOT DATA FOR CHART 1
 
-
     // 5.a: ADD AXES FOR CHART 1
-
 
     // 6.a: ADD LABELS FOR CHART 1
 
